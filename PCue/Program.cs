7294d3c9dab75ac00b15cdd84548abe46cabf7c0
yesung <chayscode@gmail.com>
2026-09-24 21:44:11 +0900

    fix Installer

---
+++ PCue/Program.cs
@@ -0,0 +1,16 @@
+using Velopack;
+
+namespace PCue;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        VelopackApp.Build().Run();
+
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
